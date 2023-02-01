c36ea27195c60578f05b1674f3f6a547def8a662
Manoj Patil <36538039+patilmanojk@users.noreply.github.com>
2023-02-01 13:52:11 +0530

    Delete Cowin-Notification-app directory

--- Cowin-Notification-app/Utils/Utils.py
+++
@@ -1,4 +0,0 @@
-class Utils():
-    chrome_driver_path = '/Users/manoj/PycharmProjects/OrangeHRM/Driver/chromedriver'
-    AUT_link = 'https://pubpeer.com/'
-    data_sheet_path = '/Users/manoj/PycharmProjects/PubPeer/data/PubPeerList.xlsx'
